dad65a5b303e67a18fd057971e5a7b8bb417847a
hamzaelk04 <hamzakabraoui07@gmail.com>
2026-09-21 11:13:51 +0000

    add the db infos in application.properties

--- src/main/java/Config/DatabaseInitializer.java
+++ src/main/java/Config/DatabaseInitializer.java
@@ -1,16 +1,23 @@
 package Config;
 
-import  org.flywaydb.core.Flyway;
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.FlywayException;
 
 public class DatabaseInitializer {
     public static void initializeSchema() {
         Flyway flyway = Flyway.configure()
                 .dataSource(
-                      DatabaseConfig.getUrl(),
-                      DatabaseConfig.getUser(),
-                      DatabaseConfig.getPassword()
+                        DatabaseConfig.getUrl(),
+                        DatabaseConfig.getUser(),
+                        DatabaseConfig.getPassword()
                 )
                 .load();
-        flyway.migrate();
+
+        try {
+            flyway.migrate();
+        } catch (FlywayException e) {
+            System.out.println("The migration doesn't work");
+            throw new RuntimeException(e);
+        }
     }
 }
