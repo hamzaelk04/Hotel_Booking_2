b58a41838a883ef574943f87da0c23e9349c2d90
hamzaelk04 <hamzakabraoui07@gmail.com>
2026-09-21 16:44:04 +0000

    Add the prompt Util

---
+++ src/main/java/Util/PromptUtil.java
@@ -0,0 +1,31 @@
+package Util;
+
+public class PromptUtil {
+    public static String namePrompt() {
+        return """
+                Enter your name: \n
+                ======================== \n
+                """;
+    }
+
+    public static String emailPrompt() {
+        return """
+                Enter your email: \n
+                ======================== \n
+                """;
+    }
+
+    public static String phonePrompt() {
+        return """
+                Enter your number phone: \n
+                ======================== \n
+                """;
+    }
+
+    public static String passwordPrompt() {
+        return """
+                Enter your password: \n
+                ======================== \n
+                """;
+    }
+}
