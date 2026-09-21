67cd42efd642050c11a80fedbb48bca922e3a651
hamzaelk04 <hamzakabraoui07@gmail.com>
2026-09-21 16:49:54 +0000

    create the user Repository and add the signatures

---
+++ src/main/java/Repository/UserRepository.java
@@ -0,0 +1,19 @@
+package Repository;
+
+import Model.User;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository {
+    void save(User user);
+
+    Optional<User> findById(UUID id);
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    List<User> findAll();
+}
