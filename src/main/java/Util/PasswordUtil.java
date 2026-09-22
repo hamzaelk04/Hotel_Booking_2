e4dd0527fe9b100fca278327a0ecf0fc7296c740
hamzaelk04 <hamzakabraoui07@gmail.com>
2026-09-22 09:58:28 +0000

    add the dependency in pom.xml of hashPw for using BCrypt and create PwUtil to add hashPw Method and checkPw method

---
+++ src/main/java/Util/PasswordUtil.java
@@ -0,0 +1,18 @@
+package Util;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordUtil {
+
+    public static String hashPassword(String plainPassword) {
+        return BCrypt.hashpw(plainPassword, BCrypt.gensalt());
+    }
+
+    public static boolean checkPassword(String plainPassword, String hashedPassword) {
+        if (hashedPassword == null || !hashedPassword.startsWith("$2a$")) {
+            return false;
+        }
+
+        return BCrypt.checkpw(plainPassword, hashedPassword);
+    }
+}
